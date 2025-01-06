ec3b17db18197c0fc97267a71777488be8d4c251
Izkyh <eizkyh@gmail.com>
2025-01-06 18:05:30 +0700

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:logica_mobile/dashboard.dart';
 import 'package:logica_mobile/login_page.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginPage(),
+      home: const Dashboard(),
     );
   }
 }
