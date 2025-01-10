a962f04d662ed6ab28bde17abd2e2515f9086c53
RaydenXVII <wantedgamer03@gmail.com>
2025-01-10 13:27:29 +0700

    Add: Page Mekanik

--- lib/pages_driver/maintenance.dart
+++ lib/pages_driver/maintenance.dart
@@ -3,7 +3,7 @@ import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 
 import 'package:logica_mobile/pages_driver/supir_dashboard.dart';
-import 'package:logica_mobile/notification.dart'; 
+import 'package:logica_mobile/notification.dart';
 
 class Maintenance extends StatefulWidget {
   const Maintenance({super.key});
@@ -33,6 +33,7 @@ class _MaintenanceState extends State<Maintenance> {
       });
     }
   }
+
   void _navigateBack(BuildContext context) {
     Navigator.pushReplacement(
       context,
@@ -47,73 +48,46 @@ class _MaintenanceState extends State<Maintenance> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xFF3B6BA7),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.white),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => SupirDashboard(
-                  notificationService: _notificationService,
+        appBar: AppBar(
+          backgroundColor: const Color(0xFF3B6BA7),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back, color: Colors.white),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SupirDashboard(
+                    notificationService: _notificationService,
+                  ),
                 ),
-              ),
-            );
-          },
-        ),
-        title: Text(
-          widget.title,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-            fontWeight: FontWeight.w600,
+              );
+            },
           ),
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.notifications, color: Colors.white),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Icon(Icons.person, color: Colors.white),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Card(
-              elevation: 2,
-              child: Padding(
-                padding: EdgeInsets.all(20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Informasi Kendaraan',
-                      style: TextStyle(
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 8.0),
-                    Text('License Plate : BMW - Z 78123 WY'),
-                    Text('Maintenance : Service Rutin'),
-                    Text('Tanggal : 18-01-2004'),
-                    Text('Catatan Tambahan: dimana mana itu ada getah'),
-                    SizedBox(height: 16.0),
-                  ],
-                ),
-              ),
+          title: Text(
+            widget.title,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
             ),
-            Container(
-              width: double.infinity,
-              padding: EdgeInsets.all(12.0),
-              child: Card(
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.notifications, color: Colors.white),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: const Icon(Icons.person, color: Colors.white),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Card(
                 elevation: 2,
                 child: Padding(
                   padding: EdgeInsets.all(20.0),
@@ -121,111 +95,140 @@ class _MaintenanceState extends State<Maintenance> {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        'Dikerjakan oleh Supaidi',
+                        'Informasi Kendaraan',
                         style: TextStyle(
+                          fontSize: 18.0,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
+                      SizedBox(height: 8.0),
+                      Text('License Plate : BMW - Z 78123 WY'),
+                      Text('Maintenance : Service Rutin'),
+                      Text('Tanggal : 18-01-2004'),
+                      Text('Catatan Tambahan: dimana mana itu ada getah'),
+                      SizedBox(height: 16.0),
                     ],
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: 16.0),
-            Text(
-              'Foto Kendaraan',
-              style: TextStyle(
-                fontSize: 16.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 8.0),
-            GestureDetector(
-              onTap: _pickImage,
-              child: Container(
-                height: 200.0,
+              Container(
                 width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                  image: _selectedImage != null
-                      ? DecorationImage(
-                          image: FileImage(_selectedImage!),
-                          fit: BoxFit.cover,
+                padding: const EdgeInsets.all(12.0),
+                child: const Card(
+                  elevation: 2,
+                  child: Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Dikerjakan oleh Supaidi',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              const Text(
+                'Foto Kendaraan',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 8.0),
+              GestureDetector(
+                onTap: _pickImage,
+                child: Container(
+                  height: 200.0,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    image: _selectedImage != null
+                        ? DecorationImage(
+                            image: FileImage(_selectedImage!),
+                            fit: BoxFit.cover,
+                          )
+                        : null,
+                  ),
+                  child: _selectedImage == null
+                      ? Icon(
+                          Icons.camera_alt,
+                          size: 50.0,
+                          color: Colors.grey[700],
                         )
                       : null,
                 ),
-                child: _selectedImage == null
-                    ? Icon(
-                        Icons.camera_alt,
-                        size: 50.0,
-                        color: Colors.grey[700],
-                      )
-                    : null,
               ),
+              const Spacer(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(
+                          context, '/SupirDashboard');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromRGBO(43, 87, 154, 1),
+                      foregroundColor: Colors.white,
+                    ),
+                    child: const Text('Kembali'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: const Text('Konfirmasi'),
+                            content: const Text(
+                                'Apakah Anda yakin ingin menyelesaikan maintenance?'),
+                            actions: [
+                              TextButton(
+                                onPressed: () => Navigator.pop(context),
+                                child: const Text('Batal'),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                  _notificationService.showNotification();
+                                  _navigateBack(context);
+                                },
+                                child: const Text('Ya'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromRGBO(43, 87, 154, 1),
+                      foregroundColor: Colors.white,
+                    ),
+                    child: const Text('Selesai'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: 0,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
             ),
-         const Spacer(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('Kembali'),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromRGBO(43, 87, 154, 1),
-                    foregroundColor: Colors.white,
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          title: const Text('Konfirmasi'),
-                          content: const Text('Apakah Anda yakin ingin menyelesaikan maintenance?'),
-                          actions: [
-                            TextButton(
-                              onPressed: () => Navigator.pop(context),
-                              child: const Text('Batal'),
-                            ),
-                            TextButton(
-                              onPressed: () {
-                                Navigator.pop(context); 
-                                _notificationService.showNotification(); 
-                                _navigateBack(context);
-                              },
-                              child: const Text('Ya'),
-                            ),
-                          ],
-                        );
-                      },
-                    );
-                  },
-                  child: const Text('Selesai'),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromRGBO(43, 87, 154, 1),
-                    foregroundColor: Colors.white,
-                  ),
-                ),
-              ],
+            BottomNavigationBarItem(
+              icon: Icon(Icons.mail),
+              label: 'Maintenance',
             ),
           ],
-        ),
-      ),
-       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.mail),
-            label: 'Maintenance',
-          ),
-        ],
-        )
-    );
+        ));
   }
 }
-
